3ac145cb8d34d853ef6a2f465c55eb1dde1d4b9d
deanrav <deanrav0@gmail.com>
2026-06-12 15:03:00 +0700

    Membuat CRUD Form Menu Anggota Membuat Fungsi Halaman di Menu Anggota Membuat Fungsi Proses Login

--- src/Koneksi/koneksi.java
+++ src/Koneksi/koneksi.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Koneksi;
 
-/**
- *
- * @author rafli
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class koneksi {
+    private static Connection conn;
     
+    public static Connection getConnection(){
+        if (conn==null) {
+            try {
+                String url = "";
+                String user = "root";
+                String pass ="";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                conn = (Connection) DriverManager.getConnection(url,user,pass);
+            } catch (Exception e) {
+                Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return conn;
+    }
 }
